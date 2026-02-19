3f8d6ea004abcc6b7c39e177e9eb55e1d993f768
Samantha Yang <samanthayang07@yahoo.com>
2026-02-19 00:21:43 -0800

    style: update UI consistency across pages and add particles bg

---
+++ app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('hero-particles', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
